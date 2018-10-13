3ee6de690f1531b30a4ae6402ede6589adfef1b4
JiltedSchimitar <44110637+JiltedSchimitar@users.noreply.github.com>
2018-10-13 13:13:08 +0530

    01. Persistent-Bugger

---
+++ 01.Persistent-Bugger/src/main/java/com/codeba/persistent/PersistentBugger.java
@@ -0,0 +1,43 @@
+package com.codeba.persistent;
+
+import java.util.stream.Stream;
+
+/**
+ * Write a function, persistence, that takes in a positive parameter num and
+ * returns its multiplicative persistence, which is the number of times you must
+ * multiply the digits in num until you reach a single digit.
+ * 
+ * For example:
+ * _______________________________________________________________________________________________
+ * |																							  |
+ * |persistence(39) == 3 // because 3*9 = 27, 2*7 = 14, 1*4=4 // and 4 has only one digit		  |
+ * |																							  |
+ * |persistence(999) == 4 // because 9*9*9 = 729, 7*2*9 = 126, // 1*2*6 = 12, and finally 1*2 = 2 |
+ * |																							  |
+ * |persistence(4) == 0 // because 4 is already a one-digit number								  |
+ * |______________________________________________________________________________________________|
+ * 
+ * @author Manuhas
+ *
+ */
+
+public class PersistentBugger {
+
+	public static void main(String[] args) {
+
+		long n = 4;
+
+		int count = 0;
+
+		while (String.valueOf(n).length() > 1) {
+
+			n = Stream.of(String.valueOf(n).split("")).mapToInt(Integer::valueOf).boxed().reduce(1, (a, b) -> a * b);
+
+			count++;
+
+		}
+
+		System.out.println(count);
+
+	}
+}
